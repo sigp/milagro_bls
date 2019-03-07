cdb0a7c050c54c6e6c26087575ba0154b7459308
Paul Hauner <paul@paulhauner.com>
2019-03-08 10:02:28 +1100

    Ignore all amcl code for rustfmt

--- amcl/src/lib.rs
+++ amcl/src/lib.rs
@@ -1,13 +1,25 @@
-pub mod arch;
+#[rustfmt::skip]
 pub mod aes;
+#[rustfmt::skip]
+pub mod arch;
+#[rustfmt::skip]
 pub mod gcm;
+#[rustfmt::skip]
 pub mod hash256;
+#[rustfmt::skip]
 pub mod hash384;
+#[rustfmt::skip]
 pub mod hash512;
-pub mod rand;
-pub mod sha3;
+#[rustfmt::skip]
 pub mod nhs;
+#[rustfmt::skip]
+pub mod rand;
+#[rustfmt::skip]
+pub mod sha3;
 
-pub mod bls383;
+#[rustfmt::skip]
 pub mod bls381;
+#[rustfmt::skip]
+pub mod bls383;
+#[rustfmt::skip]
 pub mod secp256k1;
